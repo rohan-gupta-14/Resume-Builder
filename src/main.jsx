8ed40804aca2bd2637bc66331721358edcf766f5
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-06-26 22:08:00 +0530

    Roting setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+
+const router = createBrowserRouter([
+  
+  {
+    path:'/',
+    element:<App/>
+  },
+  
+  {
+    path:'/auth/sign-in',
+    element:<SignInPage/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
