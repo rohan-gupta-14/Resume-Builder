58a4c6fc50639536919ef78a18b096be9d7a2c36
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-06-29 09:44:29 +0530

    Added dashboard

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import {Button} from '../ui/button'
 import { Link } from 'react-router-dom'
+import { UserButton } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react'
 function Header() {
+   const {user, isSignedIn} = useUser();
   return (
     <div className='p-3 px-5 flex justify-between shadow-md'>
         <img src='/logo.svg' width={100} height={100}/>
-        <Link to={'/auth/sign-in'}>
+        {isSignedIn ?
+          <div className='flex gap-2 items-center'>
+            <Button variant="outline">Dashboard</Button>
+            <UserButton/>
+          </div> :
+          <Link to={'/auth/sign-in'}>
              <Button className='bg-gray-700'>Get Started</Button>
-        </Link>
+          </Link>
+      
+        }
+        
     </div>
   )
 }
+
 
 export default Header
