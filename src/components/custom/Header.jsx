4f1a1d85a6d12df849dc3c4b9b1f919ee7bd6dc7
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2025-06-29 09:28:08 +0530

    Added Button

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import {Button} from '../ui/button'
+import { Link } from 'react-router-dom'
 function Header() {
   return (
-    <div>
+    <div className='p-3 px-5 flex justify-between shadow-md'>
         <img src='/logo.svg' width={100} height={100}/>
+        <Link to={'/auth/sign-in'}>
+             <Button className='bg-gray-700'>Get Started</Button>
+        </Link>
     </div>
   )
 }
